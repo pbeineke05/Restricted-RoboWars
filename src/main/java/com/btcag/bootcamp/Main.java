14e7a01ee76b110c3e03dc55ea055aea6e72739d
Beineke <piet.beineke@btc-ag.com>
2024-09-23 10:10:31 +0200

    startsequenz completed

--- src/main/java/com/btcag/bootcamp/Main.java
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -1,7 +1,29 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Herzlich Willkommen Spieler!");
+        System.out.println("Wie lautet dein Name?");
+        String userName = scanner.nextLine();
+
+        System.out.println("Viel Spaß beim Spielen " + userName + "!");
+        System.out.println("                                  .''.");
+        System.out.println("      .''.      .        *''*    :_\\/_:     .");
+        System.out.println("     :_\\/_:   _\\(/_  .:.*_\\/_*   : /\\ :  .'.:.'.");
+        System.out.println(" .''.: /\\ :   ./)\\   ':'* /\\ * :  '..'.  -=:o:=-");
+        System.out.println(":_\\/_:'.:::.    ' *''*    * '.\\'/.' _\\(/_'.':'.'");
+        System.out.println(": /\\ : :::::     *_\\/_*     -= o =-  /)\\    '  *");
+        System.out.println(" '..'  ':::'     * /\\ *     .'/.\\'.   '");
+        System.out.println("     *            *..*         :");
+        System.out.println();
+
+        System.out.println("Drücke eine beliebige Taste zum Fortfahren");
+        scanner.nextLine();
+
+
     }
 }
